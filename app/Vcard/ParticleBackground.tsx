83a75d312f2d057a6e698da84858fd7b216f131c
boy-whocode <naamrahulsharma@gmail.com>
2023-07-01 19:13:38 +0530

    favicon

--- app/Vcard/ParticleBackground.tsx
+++ app/Vcard/ParticleBackground.tsx
@@ -6,11 +6,11 @@ import { loadFull } from "tsparticles"
 import particleConfig from "../../config/particleConfig.json"
 
 const ParticleBackground:React.FC = ():JSX.Element=>{
-    const particleInit = useCallback(async (engine)=>{
+    const particleInit = useCallback(async (engine:any)=>{
         console.log(engine);
         await loadFull(engine)
     },[])
-    const particleLoaded = useCallback(async (container)=>{
+    const particleLoaded = useCallback(async (container:any)=>{
         await console.log(container);
         
     },[])
@@ -22,93 +22,6 @@ const ParticleBackground:React.FC = ():JSX.Element=>{
             init={particleInit}
             loaded={particleLoaded}
             options={particleConfig}
-            // options={{
-            //     background: {
-            //         color: {
-            //           value: "#000"
-            //         },
-            //         image: "",
-            //         position: "",
-            //         repeat: "",
-            //         size: "",
-            //         opacity: 1
-            //       },
-            //       backgroundMask: {
-            //         composite: "destination-out",
-            //         cover: {
-            //           color: {
-            //             value: "#fff"
-            //           },
-            //           opacity: 1
-            //         },
-            //         enable: false
-            //       },
-            //     fpsLimit: 120,
-            //     interactivity: {
-            //         events: {
-            //             onClick: {
-            //                 enable: false,
-            //                 mode: "push",
-            //             },
-            //             onHover: {
-            //                 enable: false,
-            //                 mode: "repulse",
-            //             },
-            //             resize: true,
-            //         },
-            //         modes: {
-            //             push: {
-            //                 quantity: 4,
-            //             },
-            //             repulse: {
-            //                 distance: 200,
-            //                 duration: 0.4,
-            //             },
-            //         },
-            //     },
-            //     particles: {
-            //         color: {
-            //             value: "#ffffff",
-            //         },
-            //         links: {
-            //             color: "#ffffff",
-            //             distance: 150,
-            //             enable: true,
-            //             opacity: 0.5,
-            //             width: 1,
-            //         },
-            //         collisions: {
-            //             enable: true,
-            //         },
-            //         move: {
-            //             direction: "none",
-            //             enable: true,
-            //             outModes: {
-            //                 default: "bounce",
-            //             },
-            //             random: false,
-            //             speed: 6,
-            //             straight: false,
-            //         },
-            //         number: {
-            //             density: {
-            //                 enable: true,
-            //                 area: 800,
-            //             },
-            //             value: 80,
-            //         },
-            //         opacity: {
-            //             value: 0.5,
-            //         },
-            //         shape: {
-            //             type: "circle",
-            //         },
-            //         size: {
-            //             value: { min: 1, max: 5 },
-            //         },
-            //     },
-            //     detectRetina: true,
-            // }}
             height="50vh"
             width="100vw"
             />
